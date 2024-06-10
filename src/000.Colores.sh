ab8200c794480f7fe484fc46863b6d2bf1337792
fernando <fernando@tilde.dynv6.net>
2024-06-10 16:07:55 -0300

    Adición de 000

---
+++ src/000.Colores.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+###### 8 Colores ######
+
+reset="\e[0m"
+negro="\e[30m"
+rojo="\e[31m"
+verde="\e[32m"
+amarillo="\e[33m"
+azul="\e[34m"
+magenta="\e[35m"
+cian="\e[36m"
+gris="\e[37m"
+default="\e[39m"
+
+#echo -e "$verde Texto verde $default"
+#echo -e " Texto default"
+
