ace55433c23209cf14ab395f1782c9ddf2a5cd78
fernando <fernando@tilde.dynv6.net>
2024-06-15 07:57:04 -0300

    Actualización de 108.1.a

--- src/108.1.a.Configuracion-reloj-con-systemd.sh
+++ src/108.1.a.Configuracion-reloj-con-systemd.sh
@@ -7,25 +7,15 @@ source "${0%/*}"/000.Colores.sh
 
 ## Instalación de paquetes
 echo -e "$cian Instalando paquetes $default"
-apt install -y util-linux-extra
+###### apt install -y 'package'
 
 ## Configuración de variables
 SERVICE='systemd-timesyncd'
 timestamp=$(date +%F_%H.%M.%S)
-TIME_LOCAL=$(date)
-TIME_UTC=$(date -u)
-TIME_RTC=$(hwclock)
-TIME_DIF=$(hwclock --verbose | grep drift)
 TIME_ZONE=$(timedatectl list-timezones | grep Buenos_Aires -m 1)
 
-## Impresión de TIME local y UTC
-echo -e "$verde TIME local: $rojo $TIME_LOCAL $default"
-echo -e "$verde TIME   UTC: $rojo $TIME_UTC   $default"
-
-## Impresión de TIME del sistema
-echo -e "$verde TIME hardware: $rojo $TIME_RTC $default"
-echo -e "$verde Diferencia entre TIME local y TIME de hardware (RTC): $default"
-echo -e "$rojo $TIME_DIF $default"
+## Impresión de fecha y hora del sistema
+echo -e "$verde Fecha y hora del sistema: $default"
 timedatectl
 
 ## Respaldo de configuración
@@ -33,13 +23,6 @@ cp /etc/timezone /var/backups/timezone.$timestamp
 
 ## Modificación de configuración
 echo -e "$cian Modificando configuración $default"
-
-echo -e "$amarillo Sin systemd $default"
-###### date +%Y%m%d -s "20240615"
-ln -s /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
-hwclock --systohc
-echo $TIME_ZONE > /etc/timezone
-
 echo -e "$amarillo Con systemd $default"
 echo "
 ##########################
