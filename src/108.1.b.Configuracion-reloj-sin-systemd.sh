731cb54f4192118e41f0e35699d94240da04ace8
fernando <fernando@tilde.dynv6.net>
2024-06-15 08:10:55 -0300

    Actualización de 108.1.b

--- src/108.1.b.Configuracion-reloj-sin-systemd.sh
+++ src/108.1.b.Configuracion-reloj-sin-systemd.sh
@@ -10,13 +10,13 @@ echo -e "$cian Instalando paquetes $default"
 apt install -y util-linux-extra
 
 ## Configuración de variables
-SERVICE='systemd-timesyncd'
+SERVICE=''
 timestamp=$(date +%F_%H.%M.%S)
 TIME_LOCAL=$(date)
 TIME_UTC=$(date -u)
 TIME_RTC=$(hwclock)
 TIME_DIF=$(hwclock --verbose | grep drift)
-TIME_ZONE=$(timedatectl list-timezones | grep Buenos_Aires -m 1)
+TIME_ZONE=$(find /usr/share/zoneinfo/ -name Buenos_Aires)
 
 ## Impresión de TIME local y UTC
 echo -e "$verde TIME local: $rojo $TIME_LOCAL $default"
@@ -26,7 +26,6 @@ echo -e "$verde TIME   UTC: $rojo $TIME_UTC   $default"
 echo -e "$verde TIME hardware: $rojo $TIME_RTC $default"
 echo -e "$verde Diferencia entre TIME local y TIME de hardware (RTC): $default"
 echo -e "$rojo $TIME_DIF $default"
-timedatectl
 
 ## Respaldo de configuración
 cp /etc/timezone /var/backups/timezone.$timestamp
@@ -36,26 +35,17 @@ echo -e "$cian Modificando configuración $default"
 
 echo -e "$amarillo Sin systemd $default"
 ###### date +%Y%m%d -s "20240615"
-ln -s /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
+ln -s $TIME_ZONE /etc/localtime
 hwclock --systohc
-echo $TIME_ZONE > /etc/timezone
-
-echo -e "$amarillo Con systemd $default"
-echo "
-##########################
-### Editado por ~ferorge #
-##########################
-NTP=texto-plano.xyz
-" >> /etc/systemd/timesyncd.conf
-timedatectl set-ntp True
-timedatectl set-timezone $TIME_ZONE
+###### systohc: SYStem TO Hardware Clock
+echo $TIME_ZONE | cut -d / -f 5-7 > /etc/timezone
 
 ## Reinicio de servicio
 echo -e "$cian Reiniciando servicio $default"
-systemctl restart $SERVICE
+###### systemctl restart $SERVICE
 
 ## Estado de servicio
-systemctl status $SERVICE
+###### systemctl status $SERVICE
 
 ## Verificación de configuración
 echo -e "$cian Verificando configuración $default"
