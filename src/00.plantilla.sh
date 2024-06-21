1f7c90d480f0c4f08817ffb96364855c64190c68
fernando <fernando@tilde.dynv6.net>
2024-06-21 08:52:16 -0300

    Edición de 00.plantilla.sh

--- src/00.plantilla.sh
+++ src/00.plantilla.sh
@@ -1,61 +1,64 @@
 #!/bin/bash
 
-SERVICIO=servicio
+# Configuración de plantilla
 
+## Licencia y autoría
+###### Licenciado bajo GPLv3 por ~ferorge <ferorge@texto-plano.xyz>
+###### https://www.gnu.org/licenses/gpl.txt
+
+## Importación de colores
 source "${0%/*}"/000.Colores.sh
 
-timestamp=$(date +%F_%H.%M.%S)
+## Instalación de paquetes
+echo -e "$cian Instalando paquetes $default"
+###### apt install -y 'package'
 
+## Configuración de variables
+SERVICE=''
 FQDN=$(hostname -f)
 USER=''
 VAR_DIR=''
 RUN=''
-SERVICE=''
+timestamp=$(date +%F_%H.%M.%S)
 
-# Instalacion de paquetes
-echo -e "$cian Instalando paquetes $default"
-# apt install -y 'paquete'
+## Creación de usuario
+###### useradd --system --user-group --groups ssl-cert --comment $USER-daemon --home-dir $VAR_DIR --shell /usr/sbin/nologin $USER
 
-# Creación de usuario
-useradd --system --user-group --groups ssl-cert --comment $USER-daemon --home-dir $VAR_DIR --shell /usr/sbin/nologin $USER
-
-# Respaldo de configuracion
+## Respaldo de configuración
 echo -e "$cian Respaldando configuracion $default"
-# directory=''
-# file=''
-# cp $directory$file /var/backups/$file.$timestamp
+DIR=''
+FILE=''
+###### cp $DIR$FILE /var/backups/$FILE.$timestamp
 
-# Modificacion de configuracion
+## Modificación de configuración
 echo -e "$cian Modificando configuracion $default"
-# echo '
-#####################
-# Editado por FORGE #
-#####################
-#
-# ' >> $directory$file
+###### echo '
+########################
+# Editado por ~ferorge #
+########################
+###### ' >> $DIR$FILE
 
-# Endurecimiento de servicio
-sed "/\[Service\]/r /opt/template-service.txt" $SERVICE
+## Configuración de firewall
+echo -e "$cian Configurando firewall $default"
+###### ufw allow 'puerto'/'protocolo' comment $SERVICE
+
+## Endurecimiento de servicio
+sed "/\[Service\]/r ${0%/*}/template-service.txt" $SERVICE
 sed -i "s/__USER__/$USER/g" $SERVICE
 sed -i -r "s#__PATH__#$VAR_DIR#g" $SERVICE
 sed -i -r "s#__RUN__#$RUN#g" $SERVICE
 
-
-# Verificacion de configuracion
-echo -e "$cian Verificando configuracion $default"
-
-# Configuracion de firewall
-echo -e "$cian Configurando firewall $default"
-# ufw allow 'puerto'/'protocolo' comment $SERVICIO
-
-# Reinicio de servicio
-echo -e "$cian Reiniciando servicio $default"
-# systemctl restart $SERVICIO
-
-# Activacion de servicio
+## Activación de servicio
 echo -e "$cian Activando servicio $default"
-# systemctl enable $SERVICIO
+###### systemctl enable $SERVICE
 
-# Verificacion de servicio
+## Reinicio de servicio
+echo -e "$cian Reiniciando servicio $default"
+###### systemctl restart $SERVICE
+
+## Verificación de servicio
 echo -e "$cian Verificando servicio $default"
-# systemctl status $SERVICIO
+###### systemctl status $SERVICE
+
+## Verificacion de configuracion
+echo -e "$cian Verificando configuracion $default"
