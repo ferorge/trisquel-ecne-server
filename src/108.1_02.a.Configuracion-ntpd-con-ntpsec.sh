56bb90fea357745b3005650fa3d979101787813d
fernando <fernando@tilde.dynv6.net>
2024-06-24 04:53:30 -0300

    Adición de endurecimiento de servicio

--- src/108.1_02.a.Configuracion-ntpd-con-ntpsec.sh
+++ src/108.1_02.a.Configuracion-ntpd-con-ntpsec.sh
@@ -14,16 +14,24 @@ echo -e "$cian Instalando paquetes $default"
 apt install -y ntpsec
 
 ## Configuración de variables
-SERVICE='ntpd'
+UNIT='ntpsec'
+SERVICE="/lib/systemd/system/$UNIT.service"
 FQDN=$(hostname -f)
-USER=''
-VAR_DIR=''
+USER="$UNIT"
+VAR_DIR='/var/log/ntpsec/'
 RUN=''
-DIR='/etc/ntpsec/'
+DIR='/etc/$UNIT/'
 FILE='ntp.conf'
+CFG="$UNIT"
 timestamp=$(date +%F_%H.%M.%S)
 
+## Creación de usuario
+echo -e "$cian Creando usuario $default"
+mkdir -p $VAR_DIR
+###### useradd --system --user-group --groups ssl-cert --comment $USER-daemon --home-dir $VAR_DIR --shell /usr/sbin/nologin $USER
+
 ## Respaldo de configuración
+echo -e "$cian Respaldando configuración $default"
 ##### #cp $DIR$FILE /var/backups/$FILE.$timestamp
 
 ## Modificación de configuración
@@ -34,27 +42,30 @@ echo -e "$cian Modificando configuración $default"
 ##########################
 ###### " >> $DIR$FILE
 
+## Endurecimiento de servicio
+cp $SERVICE /var/backups/$UNIT.service.$timestamp
+sed -i "/\[Service\]/r ${0%/*}/00.plantilla-de-servicios-systemd.txt" $SERVICE
+sed -i "s/__USER__/$USER/g" $SERVICE
+sed -i "s/__GROUP__/$USER/g" $SERVICE
+sed -i -r "s#__PATH__#$VAR_DIR#g" $SERVICE
+###### sed -i -r "s#__RUN__#$RUN#g" $SERVICE
+sed -i "s/__CFG__/$CFG/g" $SERVICE
+
 ## Configuracion de firewall
 echo -e "$cian Configurando firewall $default"
-ufw allow 123/tcp comment $SERVICE
-
-## Endurecimiento de servicio
-sed "/\[Service\]/r ${0%/*}/00.plantilla-de-servicios-systemd.txt" $SERVICE
-sed -i "s/__USER__/$USER/g" $SERVICE
-sed -i -r "s#__PATH__#$VAR_DIR#g" $SERVICE
-sed -i -r "s#__RUN__#$RUN#g" $SERVICE
+ufw allow 123/tcp comment $UNIT
 
 ## Activación de servicio
 echo -e "$cian Activando servicio $default"
-systemctl enable $SERVICE
+systemctl enable $UNIT
 
 ## Reinicio de servicio
 echo -e "$cian Reiniciando servicio $default"
-systemctl restart $SERVICE
+systemctl restart $UNIT
 
 ## Verificación de servicio
 echo -e "$cian Verificando servicio $default"
-systemctl status $SERVICE
+systemctl status $UNIT
 
 ## Verificación de configuración
 echo -e "$cian Verificando configuración $default"
--- src/108.1_02.b.Configuracion-ntpd-con-chrony.sh
+++ src/108.1_02.b.Configuracion-ntpd-con-chrony.sh
@@ -14,12 +14,24 @@ echo -e "$cian Instalando paquetes $default"
 apt install -y chrony
 
 ## Configuración de variables
-SERVICE='chrony'
+UNIT='chrony'
+SERVICE="/lib/systemd/system/$UNIT.service"
+FQDN=$(hostname -f)
+USER="$UNIT"
+VAR_DIR='/var/log/chrony/'
+RUN=''
 DIR='/etc/'
-FILE='chrony.conf'
+FILE='$UNIT.conf'
+CFG="$UNIT"
 timestamp=$(date +%F_%H.%M.%S)
 
+## Creación de usuario
+echo -e "$cian Creando usuario $default"
+###### mkdir -p $VAR_DIR
+###### useradd --system --user-group --groups ssl-cert --comment $USER-daemon --home-dir $VAR_DIR --shell /usr/sbin/nologin $USER
+
 ## Respaldo de configuración
+echo -e "$cian Respaldando configuración $default"
 ##### #cp $DIR$FILE /var/backups/$FILE.$timestamp
 
 ## Modificación de configuración
@@ -30,21 +42,30 @@ echo -e "$cian Modificando configuración $default"
 ##########################
 ###### " >> $DIR$FILE
 
+## Endurecimiento de servicio
+cp $SERVICE /var/backups/$UNIT.service.$timestamp
+sed -i "/\[Service\]/r ${0%/*}/00.plantilla-de-servicios-systemd.txt" $SERVICE
+sed -i "s/__USER__/$USER/g" $SERVICE
+sed -i "s/__GROUP__/$USER/g" $SERVICE
+sed -i -r "s#__PATH__#$VAR_DIR#g" $SERVICE
+###### sed -i -r "s#__RUN__#$RUN#g" $SERVICE
+sed -i "s/__CFG__/$CFG/g" $SERVICE
+
 ## Configuracion de firewall
 echo -e "$cian Configurando firewall $default"
-ufw allow 123/tcp comment $SERVICE
+ufw allow 123/tcp comment $UNIT
 
 ## Activación de servicio
 echo -e "$cian Activando servicio $default"
-systemctl enable $SERVICE
+systemctl enable $UNIT
 
 ## Reinicio de servicio
 echo -e "$cian Reiniciando servicio $default"
-systemctl restart $SERVICE
+systemctl restart $UNIT
 
 ## Verificación de servicio
 echo -e "$cian Verificando servicio $default"
-systemctl status $SERVICE
+systemctl status $UNIT
 
 ## Verificación de configuración
 echo -e "$cian Verificando configuración $default"
