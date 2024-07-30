a2c7b314139f2986ddd786fdf5d2278387f31fa3
fernando <fernando@tilde.dynv6.net>
2024-07-30 06:35:25 -0300

    Adición de guion 520

---
+++ src/520.Verificar-zona-dynv6.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Actualizar zona dynv6
+
+## __Autoría y licencia__
+###### Configuración de nombres © 2024 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### []:()
+
+## __Importación de colores__
+source "${0%/*}"/000.Colores.sh
+
+## __Configuración de variables__
+HOST=$(hostname)
+FQDN=$(hostname -f)
+CURRENT_IP=$(curl -s ifconfig.me)
+
+###### Token HTTP de la cuenta en dynv6.com
+TOKEN=$(cat /var/local/dynv6_token.txt)
+
+## __Obtención de IP de la zona__
+JSON=$(curl -s \
+    -H "Authorization: Bearer $TOKEN" \
+    -H "Accept: application/json" \
+    http://dynv6.com/api/v2/zones)
+
+ZONE_IP=$(echo $JSON | grep -o '"ipv4address":"[^"]*' | grep -o '[^"]*$')
+
+## __Respaldo de configuración__
+echo -e "$cian Respaldando configuracion $default"
+DIR=''
+FILE=''
+#cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+echo -e "$cian Modificando configuracion $default"
+
+## __Verificacion de configuración__
+echo -e "$cian Verificando configuracion $default"
+
+if [[ $CURRENT_IP != $ZONE_IP ]]; then
+  logger "current IP $CURRENT_IP"
+  logger "zone IP $ZONE_IP"
+  source "${0%/*}"/109.2_01.a.Configuracion-nombre-hospedador.sh
+  source "${0%/*}"/109.2_01.b.Configuracion-nombres-red.sh
+#  source "${0%/*}"/510.Actualizar-zona-dynv6.sh
+  UPDATE=$(curl -s "http://ipv4.dynv6.com/api/update?zone=$FQDN&ipv4=auto&token=$TOKEN")
+  logger $UPDATE
+  logger $HOST $FQDN $CURRENT_IP $ZONE_IP $UPDATE
+else
+  logger 'zone equal current IP'
+fi
+
+
