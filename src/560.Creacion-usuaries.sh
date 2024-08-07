e4ee194bedb462dd2ab3999192a3f4b67df721f6
fernando <fernando@tilde.dynv6.net>
2024-08-07 17:22:06 -0300

    Adición de guion 560.

---
+++ src/560.Creacion-usuaries.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Creación de usuaries
+
+## __Autoría y licencia__
+###### Creación de usuaries © 2024 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [fuente]:(enlace)
+
+## __Importación de colores__
+source "${0%/*}"/000.Colores.sh
+
+## __Configuración de variables__
+timestamp=$(date +%F_%H.%M.%S)
+
+## __Respaldo de configuración__
+echo -e "$cian Respaldando configuración $default"
+DIR='/var/local/'
+FILE='usuaries'
+cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+echo -e "$cian Modificando configuración $default"
+toilet -W -f mini Usuaries > $DIR$FILE
+
+chown root:staff $DIR$FILE
+chmod 0664 $DIR$FILE
