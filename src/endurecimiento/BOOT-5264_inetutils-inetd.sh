445925329ab7354f25ef2d8c382045328cae1dd6
fernando <fernando@tilde.dynv6.net>
2024-08-25 07:48:24 -0300

    Primera versión..

--- src/endurecimiento/BOOT-5264_inetutils-inetd.sh
+++ src/endurecimiento/BOOT-5264_inetutils-inetd.sh
@@ -19,12 +19,17 @@ source "${0%/*}"/../000.Colores.sh
 UNIT='inetutils-inetd'
 SERVICE="/lib/systemd/system/$UNIT.service"
 USER='inetd'
-VAR_DIR='/var/log/'
+VAR_DIR='/var/log/cfingerd'
 RUN=''
 timestamp=$(date +%F_%H.%M.%S)
 
 ## Creación de usuario
-###### useradd --system --user-group --groups ssl-cert --comment $USER-daemon --home-dir $VAR_DIR --shell /usr/sbin/nologin $USER
+mkdir -p $VAR_DIR
+chown $USER:$USER $VAR_DIR
+id $USER
+if [[ $? != 0 ]];then
+  useradd --system --user-group --comment $USER-daemon --home-dir $VAR_DIR --shell /usr/sbin/nologin $USER
+fi
 
 ## __Respaldo de configuración__
 echo -e "$cian Respaldando configuración $default"
@@ -34,12 +39,11 @@ cp $SERVICE /var/backups/$UNIT.service.$timestamp
 grep -q Endurecido $SERVICE
 if [[ $? != 0 ]];then
   sed -i "/\[Service\]/r ${0%/*}/../00.plantilla-de-servicios-systemd.txt" $SERVICE
-  ###### sed -i "s/__USER__/$USER/g" $SERVICE
-  ###### sed -i "s/__GROUP__/$USER/g" $SERVICE
+  sed -i "s/User=/User=$USER/g" $SERVICE
+  sed -i "s/Group=/Group=$USER/g" $SERVICE
   ###### sed -i -r "s#__RUN__#$RUN#g" $SERVICE
   sed -i "s,/tmp,$VAR_DIR,g" $SERVICE
-  sed -i 's/CapabilityBoundingSet=/\#CapabilityBoundingSet=/g' $SERVICE
-#  sed -i 's/CapabilityBoundingSet=/CapabilityBoundingSet=~CAP_AUDIT_* CAP_FOWNER CAP_IPC_OWNER CAP_DAC_* CAP_BPF CAP_KILL CAP_FSETID CAP_SETFCAP CAP_LEASE CAP_LINUX_IMMUTABLE CAP_IPC_LOCK CAP_BLOCK_SUSPEND CAP_SYS_ADMIN CAP_SYS_RAWIO CAP_SYS_PTRACE CAP_SYS_BOOT CAP_SYS_PACCT CAP_SYS_NICE CAP_SYS_RESOURCE CAP_SYS_TTY_CONFIG CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_BROADCAST CAP_NET_RAW CAP_SETPCAP/g' $SERVICE
+#  sed -i 's/CapabilityBoundingSet=/\#CapabilityBoundingSet=/g' $SERVICE
   sed -i 's/PrivateNetwork=true/PrivateNetwork=false/g' $SERVICE
   sed -i 's/PrivateUsers=true/PrivateUsers=false/g' $SERVICE
   sed -i 's/ProtectHome=true/ProtectHome=false/g' $SERVICE
@@ -47,7 +51,7 @@ if [[ $? != 0 ]];then
 fi
 
 ## __Recarga de demonio__
-echo -e "$cian Reiniciando servicio $default"
+echo -e "$cian Recargando demonio $default"
 systemctl daemon-reload
 
 ## __Reinicio de servicio__
