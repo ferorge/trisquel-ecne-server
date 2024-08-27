e2c76be04e825b6206b15566a085283bceac0a51
fernando <fernando@tilde.dynv6.net>
2024-08-27 06:24:25 -0300

    Adición de guion 542.

---
+++ src/542.Creacion-pie.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Creación de pie
+
+## __Autoría y licencia__
+###### Creación de pie © 2024 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [fuente]:(enlace)
+
+## __Importación de colores__
+source "${0%/*}"/000.Colores.sh
+
+## __Configuración de variables__
+HOST=$(hostname -s)
+
+## __Respaldo de configuración__
+echo -e "$cian Respaldando configuración $default"
+DIR='/var/local/'
+FILE='pie'
+cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+echo -e "$cian Modificando configuración $default"
+
+echo "
+-------------------------------------------------
+$HOST | Pubnix soberano" > $DIR$FILE
+
+logger "Pie modificado por $USER"
+
+if [ $UID == 0 ]; then
+  chown root:staff $DIR$FILE
+  chmod 0664 $DIR$FILE
+fi
