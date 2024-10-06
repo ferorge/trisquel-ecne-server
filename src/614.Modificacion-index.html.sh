775aa7d49f24d9851c638f5c8a106dde4ac870e9
fernando <fernando@tilde.dynv6.net>
2024-10-06 07:17:00 -0300

    Adecuación para html.

--- src/614.Modificacion-index.html.sh
+++ src/614.Modificacion-index.html.sh
@@ -23,41 +23,44 @@ timestamp=$(date +%F_%H.%M.%S)
 echo -e "$cian Respaldando configuración $default"
 DIR='/var/www/html/'
 FILE='index.html'
+MD='/tmp/index.md'
 cp $DIR$FILE /var/local/backups/$FILE.$timestamp
 
 ## __Modificación de configuración__
 echo -e "$cian Modificando configuración $default"
-ls /var/local/saludo
+ls /var/local/saludo > /dev/null
 if [[ $? != 0 ]];then
   source "${0%/*}"/540.Creacion-saludo.sh
 fi
 
-ls /var/local/motd
+ls /var/local/motd > /dev/null
 if [[ $? != 0 ]];then
   source "${0%/*}"/542.Creacion-mensaje-del-dia.sh
 fi
 
-ls /var/local/usuaries
+ls /var/local/usuaries > /dev/null
 if [[ $? != 0 ]];then
   source "${0%/*}"/544.Creacion-usuaries.sh
 fi
 
 DIV='_______________________________________________'
 
-sed 's/######/ /g' /var/local/saludo > $DIR$FILE
-cat /var/local/motd | cowsay -f tux | sed 's/^/### /g'  >> $DIR$FILE
-echo $DIV >> $DIR$FILE
-vrms |fold -w 64 | sed 's/^/> /g' >> $DIR$FILE
-echo $DIV >> $DIR$FILE
+sed 's/######/##### /g' /var/local/saludo > $MD
+cat /var/local/motd | cowsay -f tux | sed 's/^/        /g'  >> $MD
+echo $DIV >> $MD
+vrms |fold -w 64 | sed "2,$ s/^/##### /g" >> $MD
+echo $DIV >> $MD
 echo "
-### En línea desde: $(uptime -s)" >> $DIR$FILE
-echo $DIV >> $DIR$FILE
-sed "1,4 s/^/### /g" /var/local/usuaries >> $DIR$FILE
-#sed -i "1,6 s/^/# /g" $DIR$FILE
-#sed -i "8,11 s/^/###/g" $DIR$FILE
-#sed -i "s/$DIV/## $DIV/g" $DIR$FILE
+##### __En línea desde: $(uptime -s)__" >> $MD
+echo $DIV >> $MD
+sed 's/^/        /g' /var/local/usuaries >> $MD
+echo $DIV >> $MD
+#sed "1,4 s/^/### /g" /var/local/usuaries >> $MD
+#sed -i "1,6 s/^/# /g" $MD
+#sed -i "8,11 s/^/###/g" $MD
+#sed -i "s/$DIV/## $DIV/g" $MD
 
-multimarkdown --nolabels $DIR$FILE
+multimarkdown --nolabels -o $DIR$FILE $MD
 
 #USERS_DIR='../users/'
 #USERS=$(ls $DIR$USERS_DIR)
