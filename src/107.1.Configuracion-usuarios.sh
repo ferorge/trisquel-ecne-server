b3d21ad423ac98709d41667ddf1d6f82edb84245
fernando <fernando@tilde.dynv6.net>
2024-10-28 05:26:20 -0300

    Modificación de comentarios

--- src/107.1.Configuracion-usuarios.sh
+++ src/107.1.Configuracion-usuarios.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
-source /opt/000.Colores.sh
-alias ~~~=echo
+source "${0%/*}"/000.Colores.sh
 
-# Modificacion de terminal del modo single-user
+## __Modificacion de terminal del modo single-user__
 sed -i 's/init -t1 S/init S/g' /etc/init.d/single
 
-# Eliminacion de usuarios
+## __Eliminacion de usuarios__
 echo -e "$cian Eliminando usuarios $default"
 deluser --remove-home odroid
 
-# Adicion de usuarios
+## __Adicion de usuarios__
 echo -e "$cian Adicionando usuarios $default"
 adduser fernando
 
-# Modificacion de id
-~~~
+## __Modificacion de id__
 cp /etc/passwd /var/local/backups/passwd
 sed -i 's/1001/1000/g' /etc/passwd
 cp /etc/group /var/local/backups/group
 sed -i 's/1001/1000/g' /etc/group
 chown -R fernando:fernando /home/fernando
-~~~
 
-# Verificacion de grupos
+## __Verificacion de grupos__
 grpck -r
 
-# Verificacion de passwords
+## __Verificacion de passwords__
 pwck
 
-# Modificacion de idioma
+## __Modificacion de idioma__
 update-locale LANG=es_ES.UTF-8 LANGUAGE
