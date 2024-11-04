9f70cb0a5ce6b9a6c5c0d5b4f6f9dda8f4cf288d
fernando <fernando@tilde.dynv6.net>
2024-11-04 04:57:22 -0300

    Adición de paquetes.

--- src/102.4_01.b.Instalacion-paquetes.sh
+++ src/102.4_01.b.Instalacion-paquetes.sh
@@ -20,7 +20,7 @@ apt update
 
 ## __Instalación de paquetes__
 echo -e "$cian Instalando paquetes $default"
-apt install -y aptitude screen mc htop iotop ncdu curl nmap
+apt install -y aptitude screen mc htop iotop ncdu curl nmap lolcat aewan cowsay jp2a linuxlogo neofetch screenfetch fortune pv cmatrix cbonsai scrot sl libaa-bin oneko espeak toilet toilet-fonts rig amfora gopher lynx finger exa tmux moc nano vim emacs ed micro joe lftp
 
 ##### Endurecimiento
 #apt install -y  acct sysstat auditd audispd-plugins libpam-cracklib haveged debian-goodies debsecan libpam-tmpdir apt-listbugs apt-listchanges needrestart fail2ban aide aide-common ufw clamav arpwatch arpon debsums apt-show-versions unattended-upgrades cfengine3 puppet
