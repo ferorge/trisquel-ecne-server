5881d2409e5247e31a04d27ecdbba1b56ceaa79e
fernando <fernando@tilde.dynv6.net>
2024-11-05 05:16:43 -0300

    Adición de guion 560.

---
+++ src/560.Instalacion-glow.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Instalación de glow
+
+## __Autoría y licencia__
+###### Instalación de glow © 2024 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [glow]:(https://github.com/charmbracelet/glow)
+
+## __Importación de colores__
+source "${0%/*}"/000.Colores.sh
+
+## __Instalación de paquetes__
+echo -e "$cian Instalando paquetes $default"
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+echo 'deb [trusted=yes] https://repo.charm.sh/apt/ /' | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
