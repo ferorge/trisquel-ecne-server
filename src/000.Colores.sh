2ca623d7bd48451e76d204359379c43c69ad49c2
fernando <fernando@tilde.dynv6.net>
2024-11-18 07:29:00 -0300

    Cambio a declaración de variable de entorno.

--- src/000.Colores.sh
+++ src/000.Colores.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
-###### 8 Colores ######
 
-reset="\e[0m"
-negro="\e[30m"
-rojo="\e[31m"
-verde="\e[32m"
-amarillo="\e[33m"
-azul="\e[34m"
-magenta="\e[35m"
-cian="\e[36m"
-gris="\e[37m"
-default="\e[39m"
+# Declaración de colores
 
-#echo -e "$GREEN Texto verde $DEFAULT"
-#echo -e " Texto default"
+## __Autoría y licencia__
+###### Declaración de colores © 2024 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
 
+## __Fuente__
+###### [fuente]:(enlace)
+
+## __Configuración de variables__
+timestamp=$(date +%F_%H.%M.%S)
+
+## __Respaldo de configuración__
+echo -e "$CYAN Respaldando configuración $DEFAULT"
+DIR='/etc/'
+FILE='environment'
+cp $DIR$FILE /var/local/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+echo -e "$CYAN Modificando configuración $DEFAULT"
+grep ferorge $DIR$FILE
+if [[ $? != 0 ]];then
+echo '
+########################
+# Editado por ~ferorge #
+########################
+RESET="\e[0m"
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+GRAY="\e[37m"
+DEFAULT="\e[39m"
+' >> $DIR$FILE
+fi
