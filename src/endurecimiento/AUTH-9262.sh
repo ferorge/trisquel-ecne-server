5fbe8606db4e8c2a04d943955fa63d7dc90d5c2f
ferorge <ferorge@sobnix.dynv6.net>
2025-04-05 03:29:18 +0300

    Primera confirmacion.

---
+++ src/endurecimiento/AUTH-9262.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Endurecimiento AUTH-9262
+
+## __Autoría y licencia__
+###### Endurecimiento AUTH-9262 © 2025 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [AUTH-9262]:(https://linux-audit.com/configure-the-minimum-password-length-on-linux-systems/)
+
+## __Importación de colores__
+source "${0%/*}"/../000.Colores.sh
+
+## __Configuración de variables__
+TEST='AUTH-9262'
+UNIT=''
+PKG='libpam-pwquality'
+timestamp=$(date +%F_%H.%M.%S)
+
+## __Instalación de paquetes__
+apt install -y $PKG
+
+## __Respaldo de configuración__
+DIR=''
+FILE=''
+# echo -e "$cian Respaldando $DIR$FILE $default"
+# cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+# echo -e "$cian Modificando $DIR$FILE $default"
+# mkdir -p $DIR
+# echo "
+# ########################
+# # Editado por ~ferorge #
+# ########################
+# #
+# # $TEST
+# #
+# ########################
+# " >> $DIR$FILE
+
+## __Activación de servicio__
+# echo -e "$cian Activando servicio $default"
+# systemctl enable $UNIT
+
+## __Reinicio de servicio__
+# echo -e "$cian Reiniciando servicio $default"
+# systemctl restart $UNIT
+
+## __Verificación de servicio__
+# echo -e "$cian Verificando servicio $default"
+# systemctl status $UNIT
+
+## __Verificacion de configuración__
+# echo -e "$cian Verificando configuración $default"
+
