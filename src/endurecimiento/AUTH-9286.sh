d6612494685f64d3cf39a1fe371f4a19debfd65d
ferorge <ferorge@sobnix.dynv6.net>
2025-04-05 13:45:54 +0300

    Primera confirmacion.

--- src/endurecimiento/AUTH-9286.sh
+++ src/endurecimiento/AUTH-9286.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Endurecimiento AUTH-9282
+# Endurecimiento AUTH-9286
 
 ## __Autoría y licencia__
-###### Endurecimiento AUTH-9282 © 2025 por \~ferorge
+###### Endurecimiento AUTH-9286 © 2025 por \~ferorge
 ###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
 ###### Licenciado bajo GNU Public License version 3.
 ###### Para ver una copia de esta licencia, visite:
 ###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
 
 ## __Fuente__
-###### [AUTH-9282]:(https://cisofy.com/lynis/controls/AUTH-9282/)
+###### [AUTH-9286]:(https://linux-audit.com/configure-the-minimum-password-length-on-linux-systems/)
 
 ## __Importación de colores__
 source "${0%/*}"/../000.Colores.sh
 
 ## __Configuración de variables__
-TEST='AUTH-9282'
+TEST='AUTH-9286'
 UNIT=''
 timestamp=$(date +%F_%H.%M.%S)
 
@@ -26,28 +26,24 @@ timestamp=$(date +%F_%H.%M.%S)
 
 ## __Respaldo de configuración__
 DIR='/etc/'
-FILE='shadow'
+FILE='login.defs'
 echo -e "$cian Respaldando $DIR$FILE $default"
 cp $DIR$FILE /var/backups/$FILE.$timestamp
 
 ## __Modificación de configuración__
 echo -e "$cian Modificando $DIR$FILE $default"
-EXP_USERS=$(grep 'Account without expire date' /var/log/lynis.log | cut -d : -f 4)
-for USER in $EXP_USERS
-do
-    chage -M 365 $USER
-done
-
-# mkdir -p $DIR
-# echo "
-# ########################
-# # Editado por ~ferorge #
-# ########################
-# #
-# # $TEST
-# #
-# ########################
-# " >> $DIR$FILE
+mkdir -p $DIR
+echo "
+########################
+# Editado por ~ferorge #
+########################
+#
+# $TEST
+#
+PASS_MAX_DAYS   90
+PASS_MIN_DAYS   7
+########################
+" >> $DIR$FILE
 
 ## __Activación de servicio__
 # echo -e "$cian Activando servicio $default"
@@ -63,4 +59,3 @@ done
 
 ## __Verificacion de configuración__
 # echo -e "$cian Verificando configuración $default"
-
