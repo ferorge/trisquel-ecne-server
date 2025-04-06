70b24ebb2d5f9f69d7fe01b65d1130e95b1dbb46
ferorge <ferorge@sobnix.dynv6.net>
2025-04-07 01:25:21 +0300

    Primera confirmación.

---
+++ src/endurecimiento/PKGS-7420.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Endurecimiento AUTH-9262
+
+## __Autoría y licencia__
+###### Endurecimiento AUTH-9262 © 2025 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [AUTH-9262]:(https://linux-audit.com/configure-the-minimum-password-length-on-linux-systems/)
+
+## __Importación de colores__
+source "${0%/*}"/../000.Colores.sh
+
+## __Configuración de variables__
+TEST='AUTH-9262'
+UNIT=''
+timestamp=$(date +%F_%H.%M.%S)
+
+## __Instalación de paquetes__
+PKG='unattended-upgrades'
+apt install -y $PKG
+dpkg-reconfigure --priority=low $PKG
+ln -s /bin/unattended-upgrades /etc/cron.hourly/unattended-upgrades
+
+## __Respaldo de configuración__
+# DIR=''
+# FILE=''
+# echo -e "$cian Respaldando $DIR$FILE $default"
+# cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+# echo -e "$cian Modificando $DIR$FILE $default"
+# mkdir -p $DIR
+# echo "
+# ########################
+# # Editado por ~ferorge #
+# ########################
+# #
+# # $TEST
+# #
+# ########################
+# " >> $DIR$FILE
+
+## __Activación de servicio__
+# echo -e "$cian Activando servicio $default"
+# systemctl enable $UNIT
+
+## __Reinicio de servicio__
+# echo -e "$cian Reiniciando servicio $default"
+# systemctl restart $UNIT
+
+## __Verificación de servicio__
+# echo -e "$cian Verificando servicio $default"
+# systemctl status $UNIT
+
+## __Verificacion de configuración__
+# echo -e "$cian Verificando configuración $default"
+
