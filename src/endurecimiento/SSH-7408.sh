5465db9ac1e995929940d37963fef1cd240af806
ferorge <ferorge@sobnix.dynv6.net>
2025-04-07 04:17:57 +0300

    Primera confirmación.

--- src/endurecimiento/SSH-7408.sh
+++ src/endurecimiento/SSH-7408.sh
@@ -3,14 +3,14 @@
 # Endurecimiento SSH-7408
 
 ## __Autoría y licencia__
-###### Endurecimiento SSH-7408 © 2024 por \~ferorge
+###### Endurecimiento SSH-7408 © 2025 por \~ferorge
 ###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
 ###### Licenciado bajo GNU Public License version 3.
 ###### Para ver una copia de esta licencia, visite:
 ###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
 
 ## __Fuente__
-###### [fuente]:(enlace)
+###### [SSH-7408]:(https://linux-audit.com/ssh/audit-and-harden-your-ssh-configuration/)
 
 ## __Importación de colores__
 source "${0%/*}"/../000.Colores.sh
@@ -20,14 +20,19 @@ TEST='SSH-7408'
 UNIT='sshd'
 timestamp=$(date +%F_%H.%M.%S)
 
+## __Instalación de paquetes__
+# PKG=''
+# apt install -y $PKG
+
 ## __Respaldo de configuración__
-echo -e "$cian Respaldando configuración $default"
 DIR='/etc/ssh/sshd_config.d/'
-FILE="$TEST.conf"
+FILE='SSH-7408.conf'
+echo -e "$cian Respaldando $DIR$FILE $default"
 cp $DIR$FILE /var/backups/$FILE.$timestamp
 
 ## __Modificación de configuración__
-echo -e "$cian Modificando configuración $default"
+echo -e "$cian Modificando $DIR$FILE $default"
+mkdir -p $DIR
 echo "
 ########################
 # Editado por ~ferorge #
@@ -35,31 +40,31 @@ echo "
 #
 # $TEST
 #
-AllowTcpForwarding NO
+AllowTcpForwarding no
 ClientAliveCountMax 2
 ClientAliveInterval 0
-Compression NO
+Compression no
 FingerprintHash SHA256
-GatewayPorts NO
-IgnoreRhosts YES
+GatewayPorts no
+IgnoreRhosts yes
 LoginGraceTime 120
-LogLevel VERBOSE
+LogLevel verbose
 MaxAuthTries 2
 MaxSessions 2
-PermitRootLogin NO
-PermitUserEnvironment NO
-PermitTunnel NO
+PermitRootLogin no
+PermitUserEnvironment no
+PermitTunnel no
 Port 30022
-PrintLastLog YES
-StrictModes YES
-TCPKeepAlive NO
-UseDNS NO
-X11Forwarding NO
-AllowAgentForwarding NO
+PrintLastLog yes
+StrictModes yes
+TCPKeepAlive no
+UseDNS no
+X11Forwarding no
+AllowAgentForwarding no
 AllowUsers *
-AllowGroups fernando registro users
+AllowGroups staff users
 ########################
-" > $DIR$FILE
+" >> $DIR$FILE
 
 ## __Activación de servicio__
 echo -e "$cian Activando servicio $default"
@@ -74,5 +79,5 @@ echo -e "$cian Verificando servicio $default"
 systemctl status $UNIT
 
 ## __Verificacion de configuración__
-echo -e "$cian Verificando configuración $default"
+# echo -e "$cian Verificando configuración $default"
 
