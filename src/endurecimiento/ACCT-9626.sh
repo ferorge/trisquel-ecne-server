967e2858c019af07e4a280a317696deeaf3234c2
ferorge <ferorge@sobnix.dynv6.net>
2025-04-08 11:15:33 +0300

    Corrección de prueba.

--- src/endurecimiento/ACCT-9626.sh
+++ src/endurecimiento/ACCT-9626.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Endurecimiento AUTH-9626
+# Endurecimiento ACCT-9626
 
 ## __Autoría y licencia__
-###### Endurecimiento AUTH-9626 © 2025 por \~ferorge
+###### Endurecimiento ACCT-9626 © 2025 por \~ferorge
 ###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
 ###### Licenciado bajo GNU Public License version 3.
 ###### Para ver una copia de esta licencia, visite:
 ###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
 
 ## __Fuente__
-###### [AUTH-9626]:(https://cisofy.com/lynis/controls/ACCT-9626/)
+###### [ACCT-9626]:(https://cisofy.com/lynis/controls/ACCT-9626/)
 
 ## __Importación de colores__
 source "${0%/*}"/../000.Colores.sh
 
 ## __Configuración de variables__
-TEST='AUTH-9626'
+TEST='ACCT-9626'
 UNIT=''
 timestamp=$(date +%F_%H.%M.%S)
 
