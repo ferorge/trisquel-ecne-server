b1fd29bcd5d3cf1d6ad212eda6cd1d1f51d5aafa
ferorge <ferorge@sobnix.dynv6.net>
2025-04-10 11:44:45 +0300

    Primera confirmación.

---
+++ src/endurecimiento/FILE-7524.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Endurecimiento FILE-7524
+
+## __Autoría y licencia__
+###### Endurecimiento FILE-7524 © 2025 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [FILE-7524]:(https://linux-audit.com/configure-the-minimum-password-length-on-linux-systems/)
+
+## __Importación de colores__
+source "${0%/*}"/../000.Colores.sh
+
+## __Configuración de variables__
+TEST='FILE-7524'
+UNIT=''
+timestamp=$(date +%F_%H.%M.%S)
+
+## __Instalación de paquetes__
+# PKG=''
+# apt install -y $PKG
+
+## __Respaldo de configuración__
+# DIR=''
+# FILE=''
+# echo -e "$cian Respaldando $DIR$FILE $default"
+# cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+# echo -e "$cian Modificando $DIR$FILE $default"
+# mkdir -p $DIR
+# echo "
+# ########################
+# # Editado por ~ferorge #
+# ########################
+# #
+# # $TEST
+# #
+# ########################
+# " >> $DIR$FILE
+
+## __Modificación de permisos__
+chmod 600 /etc/crontab
+chmod 600 /etc/ssh/sshd_config
+chmod 700 /etc/cron.d/
+chmod 700 /etc/cron.daily/
+chmod 700 /etc/cron.hourly/
+chmod 700 /etc/cron.weekly/
+chmod 700 /etc/cron.monthly/
+
+
+## __Activación de servicio__
+# echo -e "$cian Activando servicio $default"
+# systemctl enable $UNIT
+
+## __Reinicio de servicio__
+# echo -e "$cian Reiniciando servicio $default"
+# systemctl restart $UNIT
+
+## __Verificación de servicio__
+# echo -e "$cian Verificando servicio $default"
+# systemctl status $UNIT
+
+## __Verificacion de configuración__
+# echo -e "$cian Verificando configuración $default"
+
