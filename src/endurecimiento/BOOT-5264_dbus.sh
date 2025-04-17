47a321c6e4cd0a66a1170161538b82f726d44853
ferorge <ferorge@sobnix.dynv6.net>
2025-04-17 22:31:29 +0300

    Primera confirmación.

---
+++ src/endurecimiento/BOOT-5264_dbus.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Endurecimiento BOOT-5264 dbus
+
+## __Autoría y licencia__
+###### Endurecimiento BOOT-5264 dbus © 2024 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [fuente]:(enlace)
+
+## __Importación de colores__
+source "${0%/*}"/../000.Colores.sh
+
+## __Configuración de variables__
+UNIT='dbus'
+SERVICE="/lib/systemd/system/$UNIT.service"
+USER=''
+VAR_DIR=''
+RUN=''
+timestamp=$(date +%F_%H.%M.%S)
+
+## Creación de usuario
+###### useradd --system --user-group --groups ssl-cert --comment $USER-daemon --home-dir $VAR_DIR --shell /usr/sbin/nologin $USER
+
+## __Respaldo de configuración__
+echo -e "$cian Respaldando configuración $default"
+cp $SERVICE /var/backups/$UNIT.service.$timestamp
+
+## __Endurecimiento de servicio__
+sed -i "/\[Service\]/r ${0%/*}/00.plantilla-de-servicios-systemd.txt" $SERVICE
+sed -i "s/User=/\#User=/g" $SERVICE
+sed -i "s/Group=/\#Group=/g" $SERVICE
+sed -i 's/CapabilityBoundingSet=/CapabilityBoundingSet=~CAP_SYS_ADMIN CAP_SYS_PTRACE/g' $SERVICE
+sed -i 's/PrivateUsers=true/PrivateUsers=false/g' $SERVICE
+
+## __Recarga de servicio__
+echo -e "$cian Recargando servicio $default"
+systemctl daemon-reload
+
+## __Reinicio de servicio__
+echo -e "$cian Reiniciando servicio $default"
+systemctl restart $UNIT
+
+## __Verificación de servicio__
+echo -e "$cian Verificando servicio $default"
+systemctl status $UNIT
+
