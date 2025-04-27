e2c7970b25613e5ae10067e46acfbbdd6c97095f
ferorge <ferorge@sobnix.dynv6.net>
2025-04-27 13:22:53 +0300

    Corrección de endurecimiento.

--- src/endurecimiento/BOOT-5264_systemd-udevd.sh
+++ src/endurecimiento/BOOT-5264_systemd-udevd.sh
@@ -3,7 +3,7 @@
 # Endurecimiento BOOT-5264 systemd-udevd
 
 ## __Autoría y licencia__
-###### Endurecimiento BOOT-5264 systemd-udevd © 2024 por \~ferorge
+###### Endurecimiento BOOT-5264 systemd-udevd © 2025 por \~ferorge
 ###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
 ###### Licenciado bajo GNU Public License version 3.
 ###### Para ver una copia de esta licencia, visite:
@@ -32,10 +32,18 @@ cp $SERVICE /var/backups/$UNIT.service.$timestamp
 
 ## __Endurecimiento de servicio__
 sed -i "/\[Service\]/r ${0%/*}/00.plantilla-de-servicios-systemd.txt" $SERVICE
-sed -i "s/User=/\#User=/" $SERVICE
-sed -i "s/Group=/\#Group=/g" $SERVICE
-sed -i 's/CapabilityBoundingSet=/CapabilityBoundingSet=~CAP_SYS_ADMIN CAP_SYS_PTRACE/g' $SERVICE
-sed -i 's/ProcSubset=pid/ProcSubset=/g' $SERVICE
+sed -i 's/Type=simple/#Type=simple/g' $SERVICE
+sed -i 's/Restart=always/#Restart=always/g' $SERVICE
+sed -i 's/CapabilityBoundingSet=/CapabilityBoundingSet=~CAP_SYS_TIME CAP_WAKE_ALARM CAP_SYS_ADMIN CAP_SYS_PTRACE CAP_SETUID CAP_SETGID CAP_SETPCAP/g' $SERVICE
+sed -i 's/# IPAddressDeny=any/IPAddressDeny=any/g' $SERVICE
+sed -i 's/PrivateDevices=true/PrivateDevices=false/g' $SERVICE
+sed -i 's/PrivateNetwork=true/PrivateNetwork=false/g' $SERVICE
+sed -i 's/PrivateTmp=true/PrivateTmp=false/g' $SERVICE
+sed -i 's/PrivateUsers=true/PrivateUsers=false/g' $SERVICE
+sed -i 's/ProcSubset=pid/ProcSubset=all/g' $SERVICE
+sed -i 's/ProtectHome=true/ProtectHome=false/g' $SERVICE
+sed -i 's/SystemCallFilter=@system-service/SystemCallFilter=@system-service @module @raw-io bpf/g' $SERVICE
+sed -i 's/RestrictAddressFamilies=/RestrictAddressFamilies=AF_UNIX AF_NETLINK AF_INET AF_INET6/g' $SERVICE
 
 ## __Recarga de servicio__
 echo -e "$cian Recargando servicio $default"
