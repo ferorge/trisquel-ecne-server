fa7687ffd9fb373392315b4427e89ed77265a26f
ferorge <ferorge@sobnix.dynv6.net>
2025-05-11 14:12:47 +0300

    Primera confirmación.

---
+++ src/endurecimiento/TOOL-5120.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Endurecimiento CRYP-8005
+
+## __Autoría y licencia__
+###### Endurecimiento TOOL-5120 © 2025 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [fuente]:(enlace)
+
+## __Importación de colores__
+source "${0%/*}"/../000.Colores.sh
+
+## __Configuración de variables__
+TEST='TOOL-5120'
+UNIT=''
+timestamp=$(date +%F_%H.%M.%S)
+
+## __Instalación de paquetes__
+PKG='snort snort-common snort-doc snort-rules-default'
+apt install -y $PKG
+
+## __Respaldo de configuración__
+# DIR=''
+# FILE=''
+# echo -e "$cian Respaldando $DIR$FILE $default"
+# cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+# echo -e "$cian Modificando $DIR$FILE $default"
+# mkdir -p $DIR
+# echo "
+# ########################
+# # Editado por ~ferorge #
+# ########################
+# #
+# # $TEST
+# #
+# ########################
+# " >> $DIR$FILE
+# sed -i 's/^[^#]*32bit_api/#&/' $DIR$FILE
+
+## __Activación de servicio__
+# echo -e "$cian Activando servicio $default"
+# systemctl enable $UNIT
+
+## __Reinicio de servicio__
+# echo -e "$cian Reiniciando servicio $default"
+# systemctl restart $UNIT
+
+## __Verificación de servicio__
+# echo -e "$cian Verificando servicio $default"
+# systemctl status $UNIT
+
+## __Verificacion de configuración__
+# echo -e "$cian Verificando configuración $default"
+
