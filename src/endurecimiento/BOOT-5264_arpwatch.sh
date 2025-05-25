5bdf554830c0aecc45e6d218838069aa04c5ba40
ferorge <ferorge@sobnix.dynv6.net>
2025-05-26 01:05:51 +0300

    Primera confirmación.

---
+++ src/endurecimiento/BOOT-5264_arpwatch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Endurecimiento BOOT-5264 arpwatch
+
+## __Autoría y licencia__
+###### Endurecimiento BOOT-5264 arpwatch © 2025 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [fuente]:(enlace)
+
+## __Importación de colores__
+source "${0%/*}"/../000.Colores.sh
+
+## __Configuración de variables__
+UNIT='arpwatch@'
+SERVICE="/lib/systemd/system/$UNIT.service"
+Iface=$(netstat -i | grep -v -e lo -e Iface -e Kernel | cut -d ' ' -f 1)
+USER=''
+VAR_DIR=''
+RUN=''
+timestamp=$(date +%F_%H.%M.%S)
+
+## Creación de usuario
+###### useradd --system --user-group --groups ssl-cert --comment $USER-daemon --home-dir $VAR_DIR --shell /usr/sbin/nologin $USER
+
+## __Respaldo de configuración__
+echo -e "$cian Respaldando configuración $default"
+cp $SERVICE /var/backups/$UNIT.service.$timestamp
+
+## __Endurecimiento de servicio__
+sed -i "/\[Service\]/r ${0%/*}/00.plantilla-de-servicios-systemd.txt" $SERVICE
+sed -i 's/PrivateNetwork=true/PrivateNetwork=false/g' $SERVICE
+sed -i 's/ProtectSystem=strict/ProtectSystem=true/g' $SERVICE
+
+## __Recarga de servicio__
+echo -e "$cian Recargando servicio $default"
+systemctl daemon-reload
+
+## __Reinicio de servicio__
+echo -e "$cian Reiniciando servicio $default"
+systemctl restart $UNIT$Iface
+
+## __Verificación de servicio__
+echo -e "$cian Verificando servicio $default"
+systemctl status $UNIT$Iface
+
+
+
