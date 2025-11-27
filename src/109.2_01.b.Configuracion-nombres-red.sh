6ade35cda43d67d329da5412c17cb3d299241b0e
ferorge <ferorge@texto-plano.xyz>
2025-11-27 17:24:56 -0300

    109.2_01.b | Cambio de salida estándar a logger.

--- src/109.2_01.b.Configuracion-nombres-red.sh
+++ src/109.2_01.b.Configuracion-nombres-red.sh
@@ -3,7 +3,7 @@
 # Configuración de nombres
 
 ## __Autoría y licencia__
-###### Configuración de nombres © 2024 por \~ferorge
+###### Configuración de nombres © 2025 por \~ferorge
 ###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
 ###### Licenciado bajo GNU Public License version 3.
 ###### Para ver una copia de esta licencia, visite:
@@ -12,28 +12,24 @@
 ## __Fuente__
 ###### [LPI 109.2_01]:(https://learning.lpi.org/en/learning-materials/102-500/109/109.2/109.2_01/)
 
-## __Importación de colores__
-source "${0%/*}"/000.Colores.sh
-
 ## __Configuración de variables__
 HOST=$(hostname -s)
 FQDN=$(hostname -f)
 CURRENT_IP=$(curl -s ifconfig.me)
 
 ## __Respaldo de configuración__
-echo -e "$CYAN Respaldando configuracion $DEFAULT"
+logger "109.2_01.b | Respaldando configuracion."
 DIR='/etc/'
 FILE='hosts'
 cp $DIR$FILE /var/local/backups/$FILE.$timestamp
 
 ## __Modificación de configuración__
-echo -e "$CYAN Modificando configuracion $DEFAULT"
+logger "109.2_01.b | Modificando configuracion."
 
 sed -i "/$FQDN/d" $DIR$FILE
 echo "$CURRENT_IP	$FQDN $HOST" >> $DIR$FILE
 
 ## __Verificacion de configuracion__
-echo -e "$CYAN Verificando configuracion $DEFAULT"
+logger "109.2_01.b | Verificando configuracion."
 read ip fqdn host <<< `grep $HOST $DIR$FILE`
-logger "IP: $ip"
-logger "FQDN: $fqdn"
+logger "IP: $ip | FQDN: $fqdn"
