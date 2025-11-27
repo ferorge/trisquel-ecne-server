77218b144a88f28962f0e5ebfee2a3d35b792dfc
ferorge <ferorge@texto-plano.xyz>
2025-11-27 17:19:49 -0300

    109.2_01.a | Cambio de salida estándar a logger. Cambio de dominio.

--- src/109.2_01.a.Configuracion-nombre-hospedador.sh
+++ src/109.2_01.a.Configuracion-nombre-hospedador.sh
@@ -3,7 +3,7 @@
 # Configuración de nombres
 
 ## __Autoría y licencia__
-###### Configuración de nombres © 2024 por \~ferorge
+###### Configuración de nombres © 2025 por \~ferorge
 ###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
 ###### Licenciado bajo GNU Public License version 3.
 ###### Para ver una copia de esta licencia, visite:
@@ -13,10 +13,10 @@
 ###### [LPI 109.2_01]:(https://learning.lpi.org/en/learning-materials/102-500/109/109.2/109.2_01/)
 
 ## __Configuración de variables__
-HOST='sobnix.dynv6.net'
+HOST='sobnix.ar'
 
 ## __Respaldo de configuración__
-echo -e "$CYAN Respaldando configuracion $DEFAULT"
+logger "109.2_01.a | Respaldando configuracion."
 DIR='/etc/'
 FILE='hostname'
 cp $DIR$FILE /var/local/backups/$FILE.$timestamp
@@ -24,15 +24,15 @@ FILE='machine-info'
 cp $DIR$FILE /var/local/backups/$FILE.$timestamp
 
 ## __Modificación de configuración__
-echo -e "$CYAN Modificando configuracion $DEFAULT"
+logger "109.2_01.a | Modificando configuracion."
 hostnamectl --pretty hostname "Sobnix | Pubnix soberano"
-hostnamectl --transient hostname $FQDN
+# hostnamectl --transient hostname $FQDN
 hostnamectl --static hostname $HOST
 hostnamectl chassis server
 hostnamectl deployment production
 hostnamectl location "Lanús, Argentina"
 
 ## __Verificacion de configuracion__
-echo -e "$CYAN Verificando configuracion $DEFAULT"
-hostnamectl status
+logger "109.2_01.a |  Verificando configuracion."
+# hostnamectl status
 logger "hostname: $(hostname -s)"
