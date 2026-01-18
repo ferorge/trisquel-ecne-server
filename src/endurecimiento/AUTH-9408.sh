ca5db502d61cef7ea4edf17270ec1efb64a50db4
root <root@pureOSlanding>
2026-01-18 20:56:02 -0300

    Primera confirmacion.

---
+++ src/endurecimiento/AUTH-9408.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Endurecimiento AUTH-9408
+
+## __Autoría y licencia__
+###### Endurecimiento AUTH-9408 © 2026 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [AUTH-9408]:(https://linux-audit.com/)
+
+## __Importación de colores__
+source "${0%/*}"/../000.Colores.sh
+
+## __Configuración de variables__
+TEST='AUTH-9408'
+UNIT=''
+timestamp=$(date +%F_%H.%M.%S)
+
+## __Instalación de paquetes__
+# PKG=''
+# apt install -y $PKG
+
+## __Respaldo de configuración__
+DIR='/etc/'
+FILE='login.defs'
+echo -e "$cian Respaldando $DIR$FILE $default"
+cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+echo -e "$cian Modificando $DIR$FILE $default"
+mkdir -p $DIR
+echo "
+########################
+# Editado por ~ferorge #
+########################
+#
+# $TEST
+#
+FAILLOG_ENAB    yes
+########################
+" >> $DIR$FILE
+
+## __Activación de servicio__
+# echo -e "$cian Activando servicio $default"
+# systemctl enable $UNIT
+
+## __Reinicio de servicio__
+# echo -e "$cian Reiniciando servicio $default"
+# systemctl restart $UNIT
+
+## __Verificación de servicio__
+# echo -e "$cian Verificando servicio $default"
+# systemctl status $UNIT
+
+## __Verificacion de configuración__
+# echo -e "$cian Verificando configuración $default"
---
+++ src/endurecimiento/PKGS-7388.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Endurecimiento PKGS-7388
+
+## __Autoría y licencia__
+###### Endurecimiento PKGS-7388 © 2025 por \~ferorge
+###### [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
+###### Licenciado bajo GNU Public License version 3.
+###### Para ver una copia de esta licencia, visite:
+###### [GPLv3]:(https://www.gnu.org/licenses/gpl.txt)
+
+## __Fuente__
+###### [PKGS-7388]:(https://cisofy.com/lynis/controls/PKGS-7388/)
+
+## __Importación de colores__
+source "${0%/*}"/../000.Colores.sh
+
+## __Configuración de variables__
+TEST='PKGS-7388'
+UNIT=''
+timestamp=$(date +%F_%H.%M.%S)
+
+## __Instalación de paquetes__
+#PKG=''
+#apt install -y $PKG
+
+## __Respaldo de configuración__
+DIR='/etc/apt/sources.list.d/'
+FILE='debian.sources'
+echo -e "$cian Respaldando $DIR$FILE $default"
+cp $DIR$FILE /var/backups/$FILE.$timestamp
+
+## __Modificación de configuración__
+echo -e "$cian Modificando $DIR$FILE $default"
+mkdir -p $DIR
+
+cp /usr/share/doc/apt/examples/debian.sources $DIR$FILE
+
+#echo "
+########################
+# Editado por ~ferorge #
+########################
+#
+# $TEST
+#
+#
+########################
+#" >> $DIR$FILE
+
+## __Activación de servicio__
+# echo -e "$cian Activando servicio $default"
+# systemctl enable $UNIT
+
+## __Reinicio de servicio__
+# echo -e "$cian Reiniciando servicio $default"
+# systemctl restart $UNIT
+
+## __Verificación de servicio__
+# echo -e "$cian Verificando servicio $default"
+# systemctl status $UNIT
+
+## __Verificacion de configuración__
+# echo -e "$cian Verificando configuración $default"
+
