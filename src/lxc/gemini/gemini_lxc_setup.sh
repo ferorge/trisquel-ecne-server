9139374fc29e5fc732a0c61acbfa52fd5ff8c09a
ferorge <ferorge@sobnix.ar>
2026-08-22 09:31:13 -0300

    gemini_lxc_setup | Cambio de plantilla.

--- src/lxc/gemini/gemini_lxc_setup.sh
+++ src/lxc/gemini/gemini_lxc_setup.sh
@@ -1,67 +1,61 @@
 #!/usr/bin/env bash
 #
-## gemini_lxc_setup.sh - Guion para configurar el servidor gemini.
+## Configuración de molly-brown
 #
-### Descripción:
-#   (Completar)
+### __Descripción__
 #
-### Autoría:
+# Guión para configurar el servidor gemini.
+#
+### __Requisitos__
+#
+#### _Paquetes_
+#
+# * bash 4.0 o superior
+#
+### __Uso__
+#
+#    ./gemini_lxc_setup.sh
+#
+### __Autor__
+#
 # Configuración de molly-brown © 2026 por \~ferorge
 # [ferorge@texto-plano.xyz](mailto:ferorge@texto-plano.xyz).
 #
-### Licencia:
-# Licenciado bajo GNU Affero Public License version 3.
+### __Licencia__
+#
+# Licenciado bajo Affero GNU Public License version 3.
 # Para ver una copia de esta licencia, visite:
-# [AGPLv3]:(https://www.gnu.org/licenses/agpl.md)
+# [AGPLv3](https://www.gnu.org/licenses/agpl.md)
+#______________________________________________________________________________
 #
-### Requisitos:
-#* bash 4.0 o superior
-#
-### Configuración:
-#
-### Uso:
-#   ./gemini_lxc_setup.sh
-#
-### Comandos durante la ejecución:
-#
-#_____________________________________________________________________________
-#
-### Configuración inicial
-#
-#```bash
-set -o errexit   # Salir si un comando falla
-set -o nounset   # Error si se usa una variable no definida
-set -o pipefail  # Fallar si un comando en un pipe falla
-set -o errtrace  # Mejor manejo de errores en funciones/subshells
-#```
-#_____________________________________________________________________________
-#
-### Constantes
-#```bash
+### __Constantes__
+\
 readonly SCRIPT_NAME=$(basename "$0")
 readonly SCRIPT_DIR=$(dirname "$(realpath "$0")")
-
-source ${SCRIPT_DIR}/colors.sh
-#```
-#_____________________________________________________________________________
-#
-### Importar funciones auxiliares
-
-
-
-### Colores
-
-RED='\033[31m'    # Rojo para errores
-GREEN='\033[32m'  # Verde para éxitos
-CYAN='\033[36m'   # Celeste para configuraciones.
-RESET='\033[0m'   # Reset de color
-
+!
+### __Importar funciones auxiliares__
+\
+if [[ ! -f "${SCRIPT_DIR}/aux.sh" ]]; then
+    RED='\033[31m'    # Rojo para errores
+    RESET='\033[0m'   # Reset de color
+    echo -e "${RED}Error: Fichero aux.sh no encontrado en ${SCRIPT_DIR}${RESET}" >&2
+    exit 1
+fi
+source "${SCRIPT_DIR}/aux.sh"
+!
+### __Configuración inicial__
+\
+cfg_safe_env || {
+    echo -e "${RED}Error: No configurarse un entorno seguro.${RESET}" >&2
+    exit 1
+}
+!
 ### __Configuración de variables__
-
+\
 FQDN='sobnix.ar'
 PKG='molly-brown'
 UNIT="${PKG}@${FQDN}"
-SRV_DIR='/srv/gemini/'
+SRV_DIR='/srv/'
 USERS_DIR="/home/"
 timestamp=$(date +%F_%H.%M.%S)
 BACKUP_DIR='/var/local/backups/'
@@ -70,21 +64,24 @@ CFG_FILE="${FQDN}.conf"
 LOG_DIR='/var/log/'
 ACCESS_LOG_FILE="${LOG_DIR}${PKG}-access.log"
 ERROR_LOG_FILE="${LOG_DIR}${PKG}-error.log"
-
-## Instalación de paquetes
+!
+### __Instalación de paquetes__
+\
 echo -e "$CYAN Instalando paquetes ${RESET}"
 apt update
 apt install -y ${PKG}
-
-## __Respaldo de configuración__
+!
+### __Respaldo de configuración__
+\
 echo -e "${CYAN} Respaldando configuración ${RESET}"
 mkdir -p ${BACKUP_DIR}
 cp ${CFG_DIR}${CFG_FILE} ${BACKUP_DIR}${CFG_FILE}.${timestamp}
-
-## __Modificación de configuración__
+!
+### __Modificación de configuración__
+\
 echo -e "${CYAN} Modificando configuración ${RESET}"
-grep -q ferorge ${CFG_DIR}${CFG_FILE}
-if [[ $? != 0 ]];then
+
+if ! grep -q ferorge ${CFG_DIR}${CFG_FILE} ;then
     echo -e "${CYAN} Creando fichero ${RESET}"
     cat <<EOF > ${CFG_DIR}${CFG_FILE}
 
@@ -96,53 +93,43 @@ Port = 1965
 CertPath = '/etc/letsencrypt/live/${FQDN}/cert.pem'
 KeyPath = '/etc/letsencrypt/live/${FQDN}/privkey.pem'
 DocBase = '$SRV_DIR'
-HomeDocBase = '/home/'
+HomeDocBase = '${USERS_DIR}'
 AccessLog = '${ACCESS_LOG_FILE}'
 ErrorLog = '${ERROR_LOG_FILE}'
 EOF
 fi
-
+#
 chmod 0644 ${CFG_DIR}${CFG_FILE}
-
-## __Configuración de registro de enventos__
+!
+### __Configuración de registro de enventos__
+\
 mkdir -p /var/log/
 chmod -R 0775 /var/log/
-
+#
 touch ${ACCESS_LOG_FILE}
 chown molly-brown:adm ${ACCESS_LOG_FILE}
 chmod 0640 ${ACCESS_LOG_FILE}
-
+#
 touch ${ERROR_LOG_FILE}
 chown molly-brown:adm ${ERROR_LOG_FILE}
 chmod 0640 ${ERROR_LOG_FILE}
-
-## __Configuración de unit__
+!
+### __Configuración de unit__
+\
 sed -i "s/DynamicUser=yes/DynamicUser=no/g" /usr/lib/systemd/system/molly-brown@.service
-
-## __Modificación de index.gmi__
-#source "${0%/*}"/602.Modificacion-index.gmi.sh
-
-## __Modificación de esqueleto__
-#source "${0%/*}"/107.1_01.skel-gemini.sh
-
-## __Endurecimiento de servicio__
-#source "${0%/*}"/endurecimiento/BOOT-5264_${PKG}.sh
-
-## __Configuración de firewall__
-#echo -e "${CYAN} Configurando firewall ${RESET}"
-#ufw allow 1965/tcp comment $UNIT
-
-## __Activación de servicio__
+!
+### __Activación de servicio__
+\
 echo -e "${CYAN} Activando servicio ${RESET}"
 systemctl enable $UNIT
-
-## __Reinicio de servicio__
+!
+### __Reinicio de servicio__
+\
 echo -e "${CYAN} Reiniciando servicio ${RESET}"
 systemctl restart $UNIT
-
-## __Verificación de servicio__
+!
+### __Verificación de servicio__
+\
 echo -e "${CYAN} Verificando servicio ${RESET}"
 systemctl status $UNIT
-
-## __Verificación de configuración__
-#echo -e "${CYAN} Verificando configuración ${RESET}"
+!
